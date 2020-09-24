9a3b455a23ca60070657238f68b7fe500cb560e0
chasin87 <yasinyuksek87@gmail.com>
2020-09-24 13:43:45 +0200

    Setup sequelize cli with postgres

---
+++ seeders/20200924114022-some-users.js
@@ -0,0 +1,25 @@
+"use strict";
+const bcrypt = require("bcrypt");
+const { SALT_ROUNDS } = require("../config/constants");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          id: 1,
+          email: "info@kidsonline.nl",
+          password: bcrypt.hashSync("kids", SALT_ROUNDS),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("users", null, {});
+  },
+};

cc70cb0b8db8084928c2d098aa26fdc9858c736a
chasin87 <yasinyuksek87@gmail.com>
2020-12-14 22:35:51 +0100

    cors update

--- index.js
+++ index.js
@@ -48,18 +48,7 @@ app.use(corsMiddleWare());
 
 const cors = require("cors");
 
-var whitelist = ["https://kids-online.netlify.app/"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 /**
  * Routes

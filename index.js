f1cb3a22870053ef3a1ebc629e5ceb2a9fcb326e
chasin87 <yasinyuksek87@gmail.com>
2020-12-14 22:27:26 +0100

    cors update

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ app.use(bodyParserMiddleWare);
 const corsMiddleWare = require("cors");
 app.use(corsMiddleWare());
 
+const cors = require("cors");
+
+var whitelist = ["http://example1.com"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 /**
  * Routes
  *
@@ -80,6 +95,3 @@ app.use("/", authRouter);
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
-
-const cors = require("cors");
-app.use(cors());
